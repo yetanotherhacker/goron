6272927f5a7572b2ae8176af5b47a441f3c8f41e
yetanotherhacker <cyrrhic@gmail.com>
2015-05-24 23:42:36 -0400

    hamming distance

--- goron.go
+++ goron.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func hamming(x, y int) int {
+	var (
+		diff int	= x^y
+		sum	int		= 0
+	)
+	for diff > 0 {
+		sum += diff % 2
+		diff = diff >> 1
+	}
+	return sum
+}
+
 func main() {
-	fmt.Println("Test.")
+	fmt.Println(hamming(1,7))
 }
