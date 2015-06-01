4de8c3c047be3ce2988f50042a31e12f97a7951e
yetanotherhacker <cyrrhic@gmail.com>
2015-05-31 23:51:07 -0400

    add threshold for code distance search

--- goron.go
+++ goron.go
@@ -14,24 +14,28 @@ func hamming(x, y uint) uint {
 	return sum
 }
 
-func codeDistance(basis []uint) (string, int) {
+func codeDistance(basis []uint, threshold uint) (isValid bool, distance uint, messageStatus string) {
+	//basis of vectors represented as uints since the goal is small-dimensional search
 	if len(basis) < 2 {
-		return "Need a multi-element array for a valid basis", -1
+		return false, 0, "Need a multi-element array for a valid basis"
 	}
-	var minDistance int = (1 << 63) - 1
-	for i, va := range basis {
+	var minDistance uint = (1 << 63) - 1
+	for i, vectorA := range basis {
 		subBasis := basis[i+1:]
-		for _, vb := range subBasis {
-			vectorDistance := int(hamming(va, vb))
+		for _, vectorB := range subBasis {
+			vectorDistance := uint(hamming(vectorA, vectorB))
 			if minDistance > vectorDistance {
 				minDistance = vectorDistance
+				if (threshold > minDistance) {
+					return false, 0, "Basis distance is lower than threshold."
+				}
 			}
 		}
 	}
-	return "Linear Code Distance:", minDistance
+	return true, minDistance, "Linear Code Distance found."
 }
 
 func main() {
 	example := []uint{1, 7, 255}
-	fmt.Println(codeDistance(example))
+	fmt.Println(codeDistance(example, 0))
 }
