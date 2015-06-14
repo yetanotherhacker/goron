905e64e06560fb01e5fd0de81f34efc3fb7cdafd
yetanotherhacker <cyrrhic@gmail.com>
2015-06-13 22:26:45 -0400

    proper formatting

--- goron.go
+++ goron.go
@@ -5,11 +5,12 @@ import (
 	"log"
 )
 
-func codeDistance(basis []uint, threshold uint) (isValid bool, distance uint, messageStatus string) {
+func basisCodeDistance(basis []uint, threshold uint) (isValid bool, distance uint, messageStatus string) {
 	//basis of vectors represented as uints since the goal is small-dimensional search
 	if len(basis) < 2 {
 		return false, 0, "Need a multi-element array for a valid basis"
 	}
+	//start with maximum possible and descend
 	var minDistance uint = (1 << 63) - 1
 	for i, vectorA := range basis {
 		subBasis := basis[i+1:]
@@ -17,7 +18,7 @@ func codeDistance(basis []uint, threshold uint) (isValid bool, distance uint, me
 			vectorDistance := uint(hamming(vectorA, vectorB))
 			if minDistance > vectorDistance {
 				minDistance = vectorDistance
-				if (threshold > minDistance) {
+				if threshold > minDistance {
 					return false, 0, "Basis distance is lower than threshold."
 				}
 			}
@@ -27,15 +28,15 @@ func codeDistance(basis []uint, threshold uint) (isValid bool, distance uint, me
 }
 
 func errorHandler(isValid bool, messageStatus string) {
-	if (!isValid) {
+	if !isValid {
 		log.Fatal("Error: ", messageStatus)
 	}
 }
 
 func hamming(x, y uint) uint {
 	var (
-		diff uint	= x^y
-		sum	uint	= 0
+		diff uint = x ^ y
+		sum  uint = 0
 	)
 	for diff > 0 {
 		sum += diff % 2
@@ -46,7 +47,7 @@ func hamming(x, y uint) uint {
 
 func main() {
 	example := []uint{1, 7, 255}
-	var status, distance, msg = codeDistance(example, 0);
-	errorHandler(status, msg);
-	fmt.Println(msg, distance);
+	var status, distance, msg = basisCodeDistance(example, 0)
+	errorHandler(status, msg)
+	fmt.Println(msg, distance)
 }
